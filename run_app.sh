8054ac0c9624797c54690b688dc060b9ce3871bc
Nguyen Duc Y <ynd@hcm57network.com>
2025-07-11 11:07:00 +0700

    updated requirements and vercel

--- run_app.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-echo "Installing dependencies..."
-pip install psycopg2-binary flask openai python-dotenv reportlab numpy matplotlib
-
-echo ""
-echo "Starting Flask application..."
-python main.py
